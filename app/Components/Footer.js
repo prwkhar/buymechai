b81d6e99d7eb9a942558dff9bb1e470568f9e521
prakhar <prakharagarwal1434@gmail.com>
2025-02-27 17:57:49 +0530

    designed navbar and footer

--- app/Components/Footer.js
+++ app/Components/Footer.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <h>I am a footer</h>
-    </div>
-  )
-}
+    <footer className="bg-white shadow-sm w-full dark:bg-gray-800 absolute bottom-0 ">
+      <div className="w-full mx-auto max-w-screen-xl py-2 px-4 flex justify-between">
+        <span className="text-sm  text-gray-500 text-center dark:text-gray-400">
+          © 2025 Developer-Prakhar Agarwal{" "}
+          <a href="https://github.com/prwkhar" className="hover:underline">
+            Github
+          </a>
+          </span>
+          <div className="text-sm  text-gray-500 text-center dark:text-gray-400">All Rights Reserved.</div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- app/Components/Navbar.js
+++ app/Components/Navbar.js
@@ -2,9 +2,18 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      <h>I am a Navbar</h>
-    </div>
+    <nav className='flex justify-between p-2 px-4 text-xl bg-blue-600'>
+      <div className='font-bold'>Chai Buddy</div>
+      <div>
+        <ul className='flex space-x-2'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>SignIn</li>
+          <li>Login</li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
